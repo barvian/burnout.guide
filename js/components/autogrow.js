b9053288f84a279f7808f2fc1fa21f819aee456a
Maxwell Barvian <mbarvian@ecd-network.com>
2018-02-20 14:00:18 -0800

    Init

---
+++ js/components/autogrow.js
@@ -0,0 +1,35 @@
+;(function(window, document) {
+  window.Autogrow = function (textarea) {
+    this.el = textarea
+
+    this.handleFocus = this.handleFocus.bind(this)
+    this.handleInput = this.handleInput.bind(this)
+
+    this.el.addEventListener('focus', this.handleFocus)
+    this.el.addEventListener('input', this.handleInput)
+  }
+
+  Autogrow.prototype.handleFocus = function (event) {
+    this.el.removeEventListener('focus', this.handleFocus)
+    const savedValue = this.el.value
+    this.el.rows = this.el.rows || this.el.getAttribute('data-min-rows') || 1
+    this.el.value = ''
+    this.baseScrollHeight = this.el.scrollHeight
+    this.el.value = savedValue
+  }
+
+  Autogrow.prototype.handleInput = function (event) {
+    const minRows = this.el.getAttribute('data-min-rows') || 1
+    this.el.rows = minRows // zero out
+
+    const lineHeight = parseInt(window.getComputedStyle(this.el, null).lineHeight, 10)
+    const rows = Math.ceil((this.el.scrollHeight - this.baseScrollHeight) / lineHeight)
+    this.el.rows = minRows + rows
+  }
+
+  // Bootstrap
+  Autogrow.bootstrap = function () {
+    return Array.from(document.querySelectorAll('textarea.js-autogrow'))
+      .map(function (textarea) { return new Autogrow(textarea) })
+  }
+})(window, document);
